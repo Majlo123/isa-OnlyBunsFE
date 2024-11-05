3a038cb79f5e28cf5f0f124b75327c1c2ed41f29
Mihajlo Bogdanovic <bogdanovicmihajlo68@gmail.com>
2024-11-05 23:35:32 +0100

    feat:display of user registration information

--- Explorer/src/app/app.module.ts
+++ Explorer/src/app/app.module.ts
@@ -1,24 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';  // Dodaj ovo
-import { HttpClientModule } from '@angular/common/http';  // Za rad sa HTTP-om
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { PostListComponent } from './post-list/post-list.component';
 import { PostCreateComponent } from './post-create/post-create.component';
+import { UserAccountComponent } from './users-list/users-list.component';
+import { AppRoutingModule } from './infrastructure/routing/app-routing.module';
+import { LayoutModule } from './feature-modules/layout/layout.module';
+import { AuthGuard } from './infrastructure/auth/auth.guard';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostListComponent,
-    PostCreateComponent
+    PostCreateComponent,
+    UserAccountComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,  // Dodaj FormsModule za ngModel
-    HttpClientModule
+    FormsModule,
+    ReactiveFormsModule, // Dodaj ako koristiš reaktivne forme
+    HttpClientModule,
+    LayoutModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [AuthGuard], // Dodaj AuthGuard ako ga koristiš
   bootstrap: [AppComponent]
 })
 export class AppModule { }
