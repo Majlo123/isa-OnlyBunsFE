8d42eb4662aecf57c30606b94704d9d6b575dbff
Uros Stanisavljevic <avangerex@gmail.com>
2024-11-14 01:12:41 +0100

    Make changes for post creation functionality

--- Explorer/src/app/post.service.ts
+++ Explorer/src/app/post.service.ts
@@ -23,13 +23,20 @@ export class PostService {
   addComment(postId: number, comment: Comment): Observable<Comment> {
     return this.http.post<Comment>(`${this.apiUrl}/${postId}/comments`, comment);
   }
+
   getPostsByUserId(userId: number): Observable<Post[]> {
     return this.http.get<Post[]>(`${this.apiUrl}/user/${userId}`);
   }
+
   deletePost(id: number): Observable<void> {
     return this.http.delete<void>(`${this.apiUrl}/${id}`);
   }
+
   updatePost(postId: number, content: string): Observable<Post> {
     return this.http.put<Post>(`${this.apiUrl}/${postId}`, { description: content });
+  }
+
+  createPost(post: Post): Observable<Post> {
+    return this.http.post<Post>(this.apiUrl,  post);
   }
 }
