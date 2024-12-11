beb4e43ca3ab82b6750000ae40be212e53da7352
Mihajlo Bogdanovic <bogdanovicmihajlo68@gmail.com>
2024-12-11 03:34:30 +0100

    feat:following and following post

--- Explorer/src/app/app.module.ts
+++ Explorer/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { PostListComponent } from './post-list/post-list.component';
 import { PostCreateComponent } from './post-create/post-create.component';
+import { ProfileComponent } from './user-profile/user-profile.component';
 import { UserAccountComponent } from './users-list/users-list.component';
 import { AppRoutingModule } from './infrastructure/routing/app-routing.module';
 import { LayoutModule } from './feature-modules/layout/layout.module';
@@ -12,6 +13,7 @@ import { AuthGuard } from './infrastructure/auth/auth.guard';
 import { AuthModule } from './infrastructure/auth/auth.module';
 import { JwtInterceptor } from './infrastructure/auth/jwt/jwt.interceptor';
 import { MyPostsComponent } from './my-posts/my-posts.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -19,7 +21,8 @@ import { MyPostsComponent } from './my-posts/my-posts.component';
     PostListComponent,
     PostCreateComponent,
     UserAccountComponent,
-    MyPostsComponent
+    MyPostsComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
@@ -28,6 +31,7 @@ import { MyPostsComponent } from './my-posts/my-posts.component';
     ReactiveFormsModule, // Dodaj ako koristiš reaktivne forme
     HttpClientModule,
     LayoutModule,
+    CommonModule ,
     AppRoutingModule
   ],
   providers: [AuthGuard,
